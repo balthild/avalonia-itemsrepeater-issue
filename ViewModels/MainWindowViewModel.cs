8b00345fc4db202a2c04823c20c9f04a90cda9e0
Balthild <ibalthild@gmail.com>
2021-01-09 07:53:33 +0800

    init

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace itemsrepeaterissue.ViewModels {
+    public class MainWindowViewModel : ViewModelBase {
+        public class ItemGroup {
+            public string Title { get; }
+            public IEnumerable<string> Items { get; }
+
+            public ItemGroup(string title, IEnumerable<string> items) {
+                Title = title;
+                Items = items;
+            }
+        }
+
+        public IEnumerable<ItemGroup> ItemGroups =>
+            new[] { 20, 20, 5, 2, 2, 2 }.Select(n => {
+                return new ItemGroup(
+                    $"Group with {n} items",
+                    Enumerable.Range(0, n).Select(i => $"Item {i}")
+                );
+            });
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace itemsrepeaterissue.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
